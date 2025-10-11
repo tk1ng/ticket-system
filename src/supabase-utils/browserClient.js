7e8ddb9c52d44897199601b8b6332da46d82e141
tk1ng <marshaeking@gmail.com>
2025-10-11 14:09:20 -0500

    chore: clean up debug log

--- src/supabase-utils/browserClient.js
+++ src/supabase-utils/browserClient.js
@@ -3,4 +3,4 @@ import { createBrowserClient } from '@supabase/ssr';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const getSupabaseBrowserClient = () => createBrowserClient(supabaseUrl, supabaseKey, { auth: { debug: true } });
+export const getSupabaseBrowserClient = () => createBrowserClient(supabaseUrl, supabaseKey,);
