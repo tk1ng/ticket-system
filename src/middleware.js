986cb818dcaab319213ae14eed3641b4dc5e859f
tk1ng <marshaeking@gmail.com>
2025-11-15 15:27:55 -0600

    refactor: clean up rewrites for tenant  domain mapping

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getSupabaseReqResClient } from './supabase-utils/reqResClient';
+import { buildUrl, getHostnameAndPort } from './utils/url-helpers';
 import { TENANT_MAP } from './tenant-map';
 
 
@@ -14,26 +15,45 @@ export async function middleware(req) {
 
     const userData = await supabase.auth.getUser();
     const sessionUser = userData.data?.user;
+
+    const [hostname, port] = getHostnameAndPort(req);
+
+    if (hostname in TENANT_MAP === false) {
+        return NextResponse.rewrite(new URL('/not-found', req.url));
+    };
+
     const requestedPath = req.nextUrl.pathname;
 
-    const [tenant, ...restOfPath] = requestedPath.substr(1).split('/');
-    const applicationPath = '/' + restOfPath.join('/');
+    const tenant = TENANT_MAP[hostname];
+    const applicationPath = requestedPath;
 
-    if (!/[a-z0-9-_]+/.test(tenant) && !applicationPath === '/') {
-        return NextResponse.rewrite(new URL('/not-found', req.url));
-    }
+    // if (!/[a-z0-9-_]+/.test(tenant) && !applicationPath === '/') {
+    //     return NextResponse.rewrite(new URL('/not-found', req.url));
+    // }
 
     if (applicationPath.startsWith('/tickets')) {
         if (!sessionUser) {
-            return NextResponse.redirect(new URL(`/${tenant}/`, req.url));
+            return NextResponse.redirect(new URL('/', req.url));
         } else if (!sessionUser.app_metadata?.tenants.includes(tenant)) {
             return NextResponse.rewrite(new URL('/not-found', req.url));
         }
     } else if (applicationPath === '/') {
         if (sessionUser) {
-            return NextResponse.redirect(new URL(`${tenant}/tickets`, req.url))
+            return NextResponse.redirect(new URL(buildUrl('/tickets', tenant, req)))
         }
     }
 
-    return response.value;
+    const rewrittenResponse = NextResponse.rewrite(
+        new URL(`/${tenant}${applicationPath}${req.nextUrl.search}`, req.url),
+        {
+            request: req,
+        }
+    );
+
+    const cookiesToSet = response.value.cookies.getAll();
+    cookiesToSet.forEach(({ name, value, options }) => {
+        rewrittenResponse.cookies.set(name, value, options);
+    });
+
+    return rewrittenResponse;
 }
--- src/utils/url-helpers.js
+++ src/utils/url-helpers.js
@@ -1,7 +1,20 @@
+export function getHostnameAndPort(request) {
+    const hostnameWithPort = request.headers.get('host');
+    const [hostname, port] = hostnameWithPort.split(':');
+
+    return [hostname, port];
+}
+
 export function urlPath(applicationPath, tenant) {
-    return `/${tenant}${applicationPath}`;
+    return applicationPath;
 }
 
 export function buildUrl(applicationPath, tenant, request) {
+    const [hostname, port] = getHostnameAndPort(request);
+
+    const portSuffix = port && port != '443' ? `${port}` : '';
+    const { protocol } = request.nextUrl;
+    const tenantUrl = `${protocol}//${hostname}:${portSuffix}`;
+
     return new URL(urlPath(applicationPath, tenant), request.url);
 }
